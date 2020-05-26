1914c8cffbd2636f016d1c5bd9e3c2a491e5f106
glaucoserrano <51676475+glaucoserrano@users.noreply.github.com>
2020-05-26 11:38:49 -0300

    Node

---
+++ ExerciciosJS/Node/ModuloB.js
@@ -0,0 +1,6 @@
+module.exports = {
+   bomDia:'Bom dia',
+   boaNoites(){
+      return 'Boa noite'
+   }
+}
---
+++ ExerciciosJS/Node/ModuloCliente.js
@@ -0,0 +1,12 @@
+const ModuloA = require('./ModuloA')
+const ModuloB = require('./ModuloB')
+
+
+console.log(ModuloA.ola)
+console.log(ModuloA.bemVindo)
+console.log(ModuloA.atelogo)
+console.log(ModuloA)
+
+console.log(ModuloB.bomDia)
+console.log(ModuloB.boaNoites())
+console.log(ModuloB)
