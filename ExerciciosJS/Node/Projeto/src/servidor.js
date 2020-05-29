2302ad46b64977ce132b98fb5dafd2f731ee5847
glaucoserrano <51676475+glaucoserrano@users.noreply.github.com>
2020-05-29 14:37:27 -0300

    Node Api

--- ExerciciosJS/Node/Projeto/src/servidor.js
+++ ExerciciosJS/Node/Projeto/src/servidor.js
@@ -1,15 +1,39 @@
 const express= require('express')
+const bd = require('./bancoDados')
+const bodyParser = require('body-parser')
 
 const porta = 3003
-
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/produtos',(req,res,next) => {
-   res.send({
-      nome:"Notebook",
-      preco: 123.45
-   }) //converter para JSON automatico
+   res.send(bd.getProdutos())
 }) 
+app.get('/produtos/:id',(req,res,next) =>{
+   res.send(bd.getProduto(req.params.id))
+})
+app.post('/produtos',(req,res,next)=>{
+   const produto = bd.salvarProduto({
+      nome: req.body.nome,
+      preco: req.body.preco
+   })
+   res.send(produto)
+})
+
+app.put('/produtos/:id',(req,res,next)=>{
+   const produto = bd.salvarProduto({
+      id: req.params.id,
+      nome: req.body.nome,
+      preco: req.body.preco
+   })
+   res.send(produto)
+})
+
+app.delete('/produtos/:id',(req,res,next)=>{
+   const produto = bd.deleteProduto(req.params.id)
+   res.send(produto)
+})
 
 app.listen(porta,() => {
    console.log(`Servidor executando na porta ${porta}`)
